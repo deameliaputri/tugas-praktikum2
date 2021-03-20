195ad52fd28a56c5556285e4d8ff4f126470ca13
deameliaputri <72556021+deameliaputri@users.noreply.github.com>
2021-03-21 00:19:45 +0700

    Update nilai_siswa.php

--- nilai_siswa.php
+++ nilai_siswa.php
@@ -20,17 +20,17 @@
 
         function grade_nilai($nilai_akhir){
                 if ($nilai_akhir >= 85 && $nilai_akhir <= 100){
-                        echo 'Grade A';
+                        echo 'A';
                 }elseif ($nilai_akhir >= 70 && $nilai_akhir <= 84){
-                        echo'Grade B';
+                        echo'B';
                 }elseif ($nilai_akhir >= 56 && $nilai_akhir <= 69){
-                        echo'Grade C';
+                        echo'C';
                 }elseif ($nilai_akhir >= 36 && $nilai_akhir <= 55){
-                        echo'Grade D';
+                        echo'D';
                 }elseif ($nilai_akhir <= 35 && $nilai_akhir <= 35){
-                        echo'Grade E';
+                        echo'E';
                 }else{
-                        echo'Grade I';
+                        echo'I';
                 }
         }
 
@@ -55,19 +55,60 @@
                                 echo 'Predikat tidak diketahui';                                   
                 }
         }
+?>        
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<div class="container">
+  <h2>Hasil Sistem Penilaian</h2>
+  
+    <table class="table table-bordered">
+    <tbody>
+      <tr>
+        <td>Nama Siswa</td>
+        <td><?php echo $nama_siswa?></td>
+      </tr>
+      <tr>
+        <td>NIM</td>
+        <td><?php echo $nim?></td>
+      </tr> 
+      <tr>
+        <td>Mata Kuliah</td>
+        <td><?php echo $mata_kuliah?></td>
+      </tr>
+      <tr>
+        <td>Nilai UTS</td>
+        <td><?php echo $nilai_uts?></td>
+      </tr>
+      <tr>
+        <td>Nilai UAS</td>
+        <td><?php echo $nilai_uas?></td>
+      </tr> 
+      <tr>
+        <td>Nilai Tugas</td>
+        <td><?php echo $nilai_tugas?></td>
+      </tr> 
+      <tr>
+        <td>Nilai Keseluruhan</td>
+        <td><?php echo $nilai_akhir?></td>
+      </tr> 
+      <tr>
+        <td>Siswa Dinyatakan</td>
+        <td><?php echo nilai($nilai_akhir)?></td>
+      </tr> 
+      <tr>
+        <td>Grade Nilai</td>
+        <td><?php echo grade_nilai($nilai_akhir)?></td>
+      </tr> 
+      <tr>
+        <td>Predikat Nilai</td>
+        <td><?php echo predikat_nilai($nilai_akhir)?></td>
+      </tr>    
+    </tbody>
+  </table>
+    
+</div>
+      
 
-            echo 'Proses : ' .$proses;
-            echo '<br>Nama : '.$nama_siswa;
-            echo '<br>NIM : '.$nim;
-            echo '<br>Mata Kuliah : '.$mata_kuliah;
-            echo '<br>Nilai UTS : '.$nilai_uts;
-            echo '<br>Nilai UAS : '.$nilai_uas;
-            echo '<br>Nilai Tugas/Praktikum  : '.$nilai_tugas; 
-            echo '<br>Total : '. $nilai_akhir; 
-            echo '<br> Siswa dinyatakan ';
-            echo nilai($nilai_akhir).'<br>';
-            echo grade_nilai($nilai_akhir).'<br>';
-            echo predikat_nilai($nilai_akhir);
-            
-
-    ?>
+    
